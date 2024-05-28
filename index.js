b6ae9465b7632238a3b07aa879599c78df8c96cd
bijay-05 <hawkeyesnipert69@gmail.com>
2024-05-28 18:52:43 +0545

    feat: created index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+
+import appRouter from './routes';
+import { notFound } from './middlewares/notFound';
+import { errorHandler } from './middlewares/errorHandler';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('API is running....');
+});
+
+app.use(appRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
